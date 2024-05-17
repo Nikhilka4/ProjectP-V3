806c0cdf1ef770e04abac79749102e0203bddba8
Nikhilka4 <106381060+Nikhilka4@users.noreply.github.com>
2024-05-17 14:21:24 +0530

    main

---
+++ client/pages/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Hero = (props: Props) => {
+  return (
+    <div className='h-[calc(100vh_-_110px)] pt-[110px] mx-[5vw] bg-slate-400'>
+        <div></div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ client/pages/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    // <div className='sticky z-10 h-[110px] bg-slate-500 mx-[5vw]'>
+    <div className='sticky block z-10 top-0 bg-[#fff5ea] w-full transition-all duration-300'>
+
+    </div>
+  )
+}
+
+export default Navbar
